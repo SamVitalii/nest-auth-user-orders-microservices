e6bc5c6096fbe1a5596f57ec670f1d6af733a2e8
SamVitalii <samorodov.vitalii@gmail.com>
2024-03-19 01:01:01 +0200

    Initial commit

---
+++ apps/billing/src/billing.controller.ts
@@ -0,0 +1,20 @@
+import {Controller, Get, UseGuards} from '@nestjs/common';
+import {Ctx, EventPattern, Payload, RmqContext} from '@nestjs/microservices';
+import {RmqService, JwtAuthGuard} from '@app/common';
+import {BillingService} from './billing.service';
+
+@Controller()
+export class BillingController {
+    constructor(
+        private readonly billingService: BillingService,
+        private readonly rmqService: RmqService,
+    ) {
+    }
+
+    @EventPattern('order_created')
+    @UseGuards(JwtAuthGuard)
+    async handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+        this.billingService.bill(data);
+        this.rmqService.ack(context);
+    }
+}
